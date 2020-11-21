139a7556f4aeb591f26021e81cb6910f25652e8c
sejalg1019 <sejalg1019@gmail.com>
2020-11-21 11:28:38 -0500

    added the catapult

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        //loading images
+        this.sling1 = loadImage("sprites/sling1.png"); 
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,11 +20,29 @@ class SlingShot{
     }
 
     display(){
+        //displaying images
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            if(pointA.x<220){ //if the bird is to the left of the catapult
+                strokeWeight(4);
+                // stroke(#54270F);
+                stroke(84,39,15); //giving color to the line
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            } else{ //if the bird is to the right of the catapult
+                strokeWeight(4);
+                stroke(84,39,15);
+                // stroke(#54270F);
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+            }
+            pop();
         }
     }
     
